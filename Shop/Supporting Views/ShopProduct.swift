78371b3b6d98359bca9282fe97c56e6db12d2102
Akin <rapiddevelopa@gmail.com>
2020-10-04 07:10:02 +0100

    update

---
+++ Shop/Supporting Views/ShopProduct.swift
@@ -0,0 +1,21 @@
+//
+//  ShopProduct.swift
+//  Shop
+//
+//  Created by Admin on 10/4/20.
+//  Copyright © 2020 rapid interactive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShopProduct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopProduct_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopProduct()
+    }
+}
